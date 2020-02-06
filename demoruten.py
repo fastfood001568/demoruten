68ab094c1b4fc32c495efceba13745b9a1f5c7eb
fastfood001568 <foodfast079@gmail.com>
2020-02-06 14:40:54 +0800

    Update demoruten.py

--- demoruten.py
+++ demoruten.py
@@ -2,11 +2,10 @@
 from  selenium import webdriver
 import time
 
-
 driver=webdriver.Chrome()
 driver.get('https://www.ruten.com.tw/')
 driver.maximize_window()   
-element=driver.find_element_by_id('keyword').send_keys('iphone11')
+driver.find_element_by_id('keyword').send_keys('iphone11')
 time.sleep(1)
 driver.find_element_by_class_name('rt-header-search-submit').click()
 driver.save_screenshot('searchitems.png')
